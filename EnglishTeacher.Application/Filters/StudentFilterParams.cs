41c2c52d4bbad7c90da1c4706baba1ade7884143
Andre Cremutcho <correia.surf@yahoo.com>
2026-03-10 16:20:29 -0300

    feat: add pagination and filters for students endpoint

--- EnglishTeacher.Application/Filters/StudentFilterParams.cs
+++ EnglishTeacher.Application/Filters/StudentFilterParams.cs
@@ -1,20 +1,12 @@
+using EnglishTeacher.Application.Common;
+
 namespace EnglishTeacher.Application.Filters;
 
-public class StudentFilterParams
+public class StudentFilterParams : PaginationParams
 {
     public string? Name { get; set; }
 
     public int? Age { get; set; }
 
     public bool IncludeInactive { get; set; } = false;
-
-    public int PageNumber { get; set; } = 1;
-
-    private int _pageSize = 10;
-
-    public int PageSize
-    {
-        get => _pageSize;
-        set => _pageSize = value > 50 ? 50 : value;
-    }
 }
